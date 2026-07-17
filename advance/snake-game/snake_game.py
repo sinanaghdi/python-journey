6a36fb99815cb325bc4d14b632e4f572fd29b777
Sina Nanaqdi <nanaqdi48@gmail.com>
2026-07-17 09:44:56 -0500

    import tkinter , and const variable

---
+++ advance/snake-game/snake_game.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+GAME_WIDTH = 700
+GAME_HIGHT = 700
+SPACE_SIZE = 30
+SLOWNESS = 200
+SNAKE_COLOR = "yellow"
+FOOD_COLOR = "RED"
+
+score = 0
+direction = "down"
+#------------------------------------------
+
+
+
+
