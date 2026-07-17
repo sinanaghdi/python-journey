d8fb0410b92ca3b4e42b991eea6fea0f5ded00f5
Sina Nanaqdi <nanaqdi48@gmail.com>
2026-07-17 13:20:25 -0500

    define Snake and Food class

--- advance/snake-game/snake_game.py
+++ advance/snake-game/snake_game.py
@@ -1,4 +1,30 @@
 from tkinter import *
+from random import randint
+
+class Snake:
+    def __init__(self):
+        self.body_size = BODY_SIZE
+        self.coordinates = []
+        self.squares = []
+        
+        for i in range(0, BODY_SIZE):
+            self.coordinates.append([0,0])
+
+        for x,y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x+SPACE_SIZE , y+SPACE_SIZE,  fill=SNAKE_COLOR , tag='snake' )
+            self.squares.append(square)
+            
+class Food:
+    def __init__(self):
+        x = randint(0, (GAME_WIDTH // SPACE_SIZE)-1) * SPACE_SIZE
+        y = randint(0, (GAME_HIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
+        self.coordinates = (x,y)
+        canvas.create_rectangle(x, y, x+SPACE_SIZE , y+SPACE_SIZE,  fill=FOOD_COLOR  , tag='Food' )          
+            
+
+
+# def next_turn(snake,food):
+    
 
 
 def restart_program(arg):
@@ -6,8 +32,9 @@ def restart_program(arg):
 #-------------------------------------------
 GAME_WIDTH = 700
 GAME_HIGHT = 700
-SPACE_SIZE = 30
+SPACE_SIZE = 25
 SLOWNESS = 200
+BODY_SIZE = 2
 SNAKE_COLOR = "yellow"
 FOOD_COLOR = "RED"
 BACKGROUND_COLOR = "black"
@@ -38,4 +65,11 @@ screen_height = window.winfo_screenheight()
 x = int((screen_width / 2) - (window_height / 2))
 y= int((screen_height / 2) - (window_height / 2))
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+
+next_turn(snake, food)
+
 window.mainloop()
