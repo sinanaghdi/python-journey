99f89401f195efabe5d21a5c357b2afbd814b732
Sina Nanaqdi <nanaqdi48@gmail.com>
2026-07-18 11:19:11 -0500

    import library and install package

---
+++ advance/OCR/OCR.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import pytesseract
+import pathlib
+from googletrans import Translator
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
